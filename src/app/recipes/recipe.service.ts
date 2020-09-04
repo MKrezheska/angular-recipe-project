931dbab017119fcf91ae0b516176e07512a63ff1
Magdalena Krezheska <Magdalena.Krezheska@netcetera.com>
2020-09-04 11:31:28 +0200

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
@@ -7,9 +8,23 @@ export class RecipeService {
 
     private recipies: Recipe[] = [
         // tslint:disable-next-line: max-line-length
-        new Recipe('A Test Recipe', 'This is a test', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg'),
+        new Recipe(
+            'A Test Recipe', 
+            'This is a test', 
+            'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
         // tslint:disable-next-line: max-line-length
-        new Recipe('Another Test Recipe', 'This is a test', 'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg')
+        new Recipe(
+            'Another Test Recipe', 
+            'This is a test', 
+            'https://i1.wp.com/www.eatthis.com/wp-content/uploads/2019/10/pumpkin-pad-thai-recipe.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ]),
       ];
 
     getRecipes(): Recipe[] {
